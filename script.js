564f4788c7aafd7ea8986f90f99d7cc09c701696
Justin Elliott <thecrashtestjustin@gmail.com>
2023-03-20 08:35:36 -0700

    updates

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+formValidation();
 
+function formValidation() {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.addEventListener("focusout", (e) => {
+      if (!input.checkValidity()) {
+        showError(input);
+      } else {
+        console.log("correct");
+      }
+    });
+  });
+
+  const submit = document.querySelector(".submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("submit");
+  });
+}
+
+function showError(input) {
+  const formInputs = document.querySelectorAll("form input");
+  const errorMessages = document.querySelectorAll(".error-message");
+
+  console.log(input);
+}
