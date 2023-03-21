76dcabf4c76af57eea0d6403ee5b6258d08e491a
Justin Elliott <thecrashtestjustin@gmail.com>
2023-03-21 06:18:23 -0700

    basic form validation for empty fields & valididty

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ formValidation();
 function formValidation() {
   const inputs = document.querySelectorAll("input");
   inputs.forEach((input) => {
+    const errorMessage = input.nextElementSibling;
+    const formDivParent = input.parentNode;
     input.addEventListener("focusout", (e) => {
       if (!input.checkValidity()) {
         showError(input);
       } else {
         console.log("correct");
+        errorMessage.textContent = "";
+        input.classList.remove("invalid");
+        formDivParent.style.marginBottom = "30px";
+      }
+    });
+    input.addEventListener("keyup", (e) => {
+      if (!input.checkValidity()) {
+        showError(input);
+      } else {
+        console.log("correct");
+        errorMessage.textContent = "";
+        input.classList.remove("invalid");
+        formDivParent.style.marginBottom = "30px";
       }
     });
   });
@@ -20,8 +35,33 @@ function formValidation() {
 }
 
 function showError(input) {
-  const formInputs = document.querySelectorAll("form input");
-  const errorMessages = document.querySelectorAll(".error-message");
+  const formDivParent = input.parentNode;
+  const errorMessage = input.nextElementSibling;
 
-  console.log(input);
+  if (input.validity.valueMissing) {
+    errorMessage.textContent = "This field is required.";
+    formDivParent.style.marginBottom = "15px";
+    input.classList.add("invalid");
+    return;
+  } else {
+    formDivParent.style.marginBottom = "15px";
+    input.classList.add("invalid");
+    errorMessageDeterminant(input);
+  }
+}
+
+function errorMessageDeterminant(input) {
+  const errorMessage = input.nextElementSibling;
+
+  if (input.id === "email-input") {
+    errorMessage.textContent = "Please provide a valid email address.";
+  } else if (input.id === "country-input") {
+    errorMessage.textContent = "Please provide a valid country.";
+  } else if (input.id === "zipcode-input") {
+    errorMessage.textContent = "Please provide a valid zip code.";
+  } else if (input.id === "password") {
+    errorMessage.textContent = "Please provide a valid password.";
+  } else if (input.id === "password-confirmation") {
+    errorMessage.textContent = "Passwords do not match.";
+  }
 }
